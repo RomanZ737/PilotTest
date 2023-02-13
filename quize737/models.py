b91d348ce8dd5c9f904613a4beb5cb5eac40d40d
zaychenkoroman <po>
2023-02-13 13:05:53 +0300

    Start

--- quize737/models.py
+++ quize737/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class QuestionSet(models.Model):
+    # Задаём имена систем
+    them_names = [('Electrical', 'Electrical'), ('Engine', 'Engine'), ('Landin Gear', 'Landin Gear')]
+
+    them_name = models.CharField(max_length=500, verbose_name='Тема Вопроса', choices=them_names)
+    question = models.CharField(max_length=500, verbose_name='Вопрос')
+    option_1 = models.CharField(max_length=500, verbose_name='Вариант 1')
+    option_2 = models.CharField(max_length=500, verbose_name='Вариант 2')
+    option_3 = models.CharField(max_length=500, verbose_name='Вариант 3', blank=True, null=True)
+    option_4 = models.CharField(max_length=500, verbose_name='Вариант 4', blank=True, null=True)
+    answer = models.IntegerField(verbose_name='Ответ, в виде номера строки ответа')
+
+    class Meta:
+        ordering = ['-them_name']
+
+    def __str__(self):
+        return self.them_name, self.question
+
+
+class QuizeSet(models.Model):
+    """Варианты тестов стандарнтых тестов"""
+    quize_name = models.CharField(max_length=255, verbose_name='Название Теста')
+
+    def __str__(self):
+        return self.quize_name
+
+
--- quize737/views.py
+++ quize737/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import QuestionSet, QuizeSet
 
-# Create your views here.
+
+def index(request):
+    """Отображение домашней страницы"""
+
+    # Генерируем список доступных тестов
+    all_quiz = QuizeSet.objects.all().values()
+
+    # Отрисовка HTML-страницы с данными внутри в переменой context
+    return render(request,
+                  'index.html',
+                  context={'all_quiz': all_quiz}
+                  )
+
+
+def question(request):
+    selected_questons = [x for x in QuestionSet.objects.all()]
+    context = {'selected_questons': selected_questons}
+
+    return render(request, 'question.html', context=context)
