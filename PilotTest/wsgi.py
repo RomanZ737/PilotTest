e53df07bc05d5690467b66fbe762c145ca6e121f
zaychenkoroman <po>
2023-02-13 12:41:26 +0300

    Settings

--- PilotTest/wsgi.py
+++ PilotTest/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/etc/default/web_app/PilotTest')
+sys.path.append('/etc/default/web_app/PilotTest/PilotTest')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PilotTest.settings')
 
