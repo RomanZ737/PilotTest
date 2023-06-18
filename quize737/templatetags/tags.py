27218ba50d7a950a5d77509c2f7aed6e7231fb65
zaychenkoroman <po>
2023-06-18 22:13:58 +0300

    Start

--- quize737/templatetags/tags.py
+++ quize737/templatetags/tags.py
@@ -1,12 +1,14 @@
 from django import template
 import random
+from django.contrib.auth.models import User
 
 register = template.Library()
 
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    return user.groups.filter(name=group_name).exists()
+    str_new = ''.join(group_name).split(',')
+    return user.groups.filter(name__in=str_new).exists()
 
 
 @register.filter(name='random_loop')
