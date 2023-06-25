5bfef84e6ee5ab271baad62d91bacf11071c89c8
RomanZ737 <pomanz@mail.ru>
2023-06-25 21:23:20 +0300

    User Group Perm

--- quize737/views.py
+++ quize737/views.py
@@ -1664,7 +1664,8 @@ def file_upload(request):
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             #  Зашружаем файл
-            newdoc = FileUpload(docfile=(request.FILES['docfile']).decode('utf-8'))
+
+            newdoc = FileUpload(docfile=request.FILES['docfile'])
             newdoc.save()
             error_read = {}  # Ошибка при чтении файл
             wrong_data = []  # Ошибки в данных файла (пропущенные поля)
