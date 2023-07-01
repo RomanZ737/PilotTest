bcf37b41d32b48b943362d5f696099575846d8c9
zaychenkoroman <po>
2023-07-01 13:44:09 +0300

    Start

--- static/no_back.js
+++ static/no_back.js
@@ -35,11 +35,3 @@
     }
 })(window);
 
-//Убираем кнопку ответить и показываем надпись 'ответ принят, ожидайте...'
-
-    const btn = document.getElementById('answer_btn');
-    btn.addEventListener('click', () => {
-      btn.style.display = 'none';
-      const box = document.getElementById('wait');
-      box.style.display = 'block';
-    });
