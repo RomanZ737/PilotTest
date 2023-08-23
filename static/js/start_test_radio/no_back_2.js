9aa0f3790de305911e1f8b066a90b84e1ddf1d51
zaychenkoroman <po>
2023-08-23 12:38:05 +0300

    Start

--- static/js/start_test_radio/no_back_2.js
+++ static/js/start_test_radio/no_back_2.js
@@ -1,9 +1,9 @@
-window.location.hash = "no-back-button";
+$(document).ready(function() {
+  window.location.hash = "no-back-button";
 
-    // Again because Google Chrome doesn't insert
-    // the first hash into the history
-    window.location.hash = "Again-No-back-button";
+  window.location.hash = "Again-No-back-button"; 
 
-    window.onhashchange = function(){
-        window.location.hash = "no-back-button";
-    }
+  window.onhashchange = function(){
+    window.location.hash = "no-back-button";
+  }
+});
