4a5bb5fec963dbbd5f51b64b45c5b23b2de0e05b
RomanZ737 <pomanz@mail.ru>
2023-09-29 23:48:20 +0300

    User Group Perm

---
+++ tmp.py
@@ -0,0 +1,10 @@
+<script>
+$(document).ready(function(){ function disableBack()
+{window.history.forward()}
+
+window.onload = disableBack();
+window.onpageshow = function(evt)
+{ if (evt.persisted)
+disableBack()}
+});
+</script>
