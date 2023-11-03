9078690faa6e59a4bd8064c25c90b93305bb91c3
RomanZ737 <pomanz@mail.ru>
2023-11-03 18:57:42 +0300

    User Group Perm

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,10 @@
-from aiogram.fsm.storage.memory import MemoryStorage
+#from aiogram.fsm.storage.memory import MemoryStorage
 from decouple import config  # позволяет скрывать критическую информацию (пароли, логины, ip)
-from aiogram import Bot, Dispatcher
+#from aiogram import Bot, Dispatcher
 import telebot  # библиотека работы с телеграм
 
 
-fsm_storage = MemoryStorage()  # Хранилище состояний
+#fsm_storage = MemoryStorage()  # Хранилище состояний
 crewbot_token = config("TEST_CREW_BOT_TOKEN", default='')
 develop_chat_id = config("DEVELOP_CHAT_ID", default='')
 
