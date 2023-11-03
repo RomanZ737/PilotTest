0b19a60884b221c30bbe84fe985235d21414dbe0
RomanZ737 <pomanz@mail.ru>
2023-11-03 20:52:33 +0300

    User Group Perm

--- PilotTest/settings.py
+++ PilotTest/settings.py
@@ -29,7 +29,7 @@ secret_key = config('secret_key', default='')
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOGGING = {
     "version": 1,
